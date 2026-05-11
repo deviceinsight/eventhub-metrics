ec6e4094a3d6fd55dc04e4638ea8de68edbf7d54
Dirk Wilden <dirk.wilden@device-insight.com>
2026-05-11 14:42:25 +0200

    migrate linter

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -2,6 +2,13 @@ package metrics
 
 const metricPrefix = "eh_metrics"
 
+const (
+	labelNamespace     = "eh_namespace"
+	labelEventhub      = "eventhub"
+	labelPartitionID   = "partition_id"
+	labelConsumerGroup = "consumer_group"
+)
+
 type Metric struct {
 	Name   string
 	Help   string
@@ -11,74 +18,74 @@ type Metric struct {
 var NamespaceInfo = &Metric{
 	Name:   "namespace_info",
 	Help:   "eventhub namespace info",
-	Labels: []string{"eh_namespace", "eh_endpoint"},
+	Labels: []string{labelNamespace, "eh_endpoint"},
 }
 
 var EventhubInfo = &Metric{
 	Name:   "eventhub_info",
 	Help:   "eventhub info",
-	Labels: []string{"eh_namespace", "eventhub", "partition_count", "retention_in_days"},
+	Labels: []string{labelNamespace, labelEventhub, "partition_count", "retention_in_days"},
 }
 
 var EventhubPartitionSequenceNumberMin = &Metric{
 	Name:   "eventhub_partition_sequence_min",
 	Help:   "beginning sequence number of a partition",
-	Labels: []string{"eh_namespace", "eventhub", "partition_id"},
+	Labels: []string{labelNamespace, labelEventhub, labelPartitionID},
 }
 
 var EventhubSequenceNumberMinSum = &Metric{
 	Name:   "eventhub_sequence_min_sum",
 	Help:   "sum of all the eventhub's partition beginning sequence numbers",
-	Labels: []string{"eh_namespace", "eventhub"},
+	Labels: []string{labelNamespace, labelEventhub},
 }
 
 var EventhubPartitionSequenceNumberMax = &Metric{
 	Name:   "eventhub_partition_sequence_max",
 	Help:   "last enqueued sequence number of a partition",
-	Labels: []string{"eh_namespace", "eventhub", "partition_id"},
+	Labels: []string{labelNamespace, labelEventhub, labelPartitionID},
 }
 
 var EventhubSequenceNumberMaxSum = &Metric{
 	Name:   "eventhub_sequence_max_sum",
 	Help:   "sum of all the eventhub's partition last enqueued sequence numbers",
-	Labels: []string{"eh_namespace", "eventhub"},
+	Labels: []string{labelNamespace, labelEventhub},
 }
 
 var ConsumerGroupInfo = &Metric{
 	Name:   "consumer_group_info",
 	Help:   "consumer group info gauges. It will report 1 if the group is in the stable state, otherwise 0.",
-	Labels: []string{"eh_namespace", "eventhub", "consumer_group", "state"},
+	Labels: []string{labelNamespace, labelEventhub, labelConsumerGroup, "state"},
 }
 
 var ConsumerGroupOwners = &Metric{
 	Name:   "consumer_group_owners",
 	Help:   "consumer group owner count gauges. It will report the number of owners in the consumer group",
-	Labels: []string{"eh_namespace", "eventhub", "consumer_group"},
+	Labels: []string{labelNamespace, labelEventhub, labelConsumerGroup},
 }
 
 var ConsumerGroupEventsSum = &Metric{
 	Name:   "consumer_group_events_sum",
 	Help:   "the sum of all committed sequence numbers across all partitions in an eventhub",
-	Labels: []string{"eh_namespace", "eventhub", "consumer_group"},
+	Labels: []string{labelNamespace, labelEventhub, labelConsumerGroup},
 }
 
 var ConsumerGroupPartitionOwner = &Metric{
 	Name:   "consumer_group_partition_owner",
 	Help:   "info about owner of a partition in a consumer group. Value is 0 if owner is expired, otherwise 1.",
-	Labels: []string{"eh_namespace", "eventhub", "consumer_group", "partition_id", "owner"},
+	Labels: []string{labelNamespace, labelEventhub, labelConsumerGroup, labelPartitionID, "owner"},
 }
 
 var ConsumerGroupPartitionLag = &Metric{
 	Name: "consumer_group_partition_lag",
 	Help: "the number of messages a consumer group is lagging behind the last enqueued sequence number" +
 		" of a partition",
-	Labels: []string{"eh_namespace", "eventhub", "consumer_group", "partition_id"},
+	Labels: []string{labelNamespace, labelEventhub, labelConsumerGroup, labelPartitionID},
 }
 
 var ConsumerGroupLag = &Metric{
 	Name:   "consumer_group_lag",
 	Help:   "the number of messages a consumer group is lagging behind across all partitions in an eventhub",
-	Labels: []string{"eh_namespace", "eventhub", "consumer_group"},
+	Labels: []string{labelNamespace, labelEventhub, labelConsumerGroup},
 }
 
 var allMetrics = []*Metric{NamespaceInfo, EventhubInfo, EventhubPartitionSequenceNumberMin,
--- internal/metrics/service.go
+++ internal/metrics/service.go
@@ -29,15 +29,15 @@ type service struct {
 
 func (s *service) RecordNamespaceInfo(namespace, endpoint string) {
 	s.recorder.RecordMetric(NamespaceInfo, map[string]string{
-		"eh_namespace": namespace,
+		labelNamespace: namespace,
 		"eh_endpoint":  endpoint},
 		1.0)
 }
 
 func (s *service) RecordEventhubInfo(namespace, eventhub string, partitionCount int, messageRetentionInDays int) {
 	s.recorder.RecordMetric(EventhubInfo, map[string]string{
-		"eh_namespace":      namespace,
-		"eventhub":          eventhub,
+		labelNamespace:      namespace,
+		labelEventhub:       eventhub,
 		"partition_count":   fmt.Sprintf("%d", partitionCount),
 		"retention_in_days": fmt.Sprintf("%d", messageRetentionInDays)},
 		1.0)
@@ -45,27 +45,27 @@ func (s *service) RecordEventhubInfo(namespace, eventhub string, partitionCount 
 
 func (s *service) RecordEventhubPartitionSequenceNumber(namespace, eventhub, partitionID string, seqMin, seqMax int64) {
 	s.recorder.RecordMetric(EventhubPartitionSequenceNumberMin, map[string]string{
-		"eh_namespace": namespace,
-		"eventhub":     eventhub,
-		"partition_id": partitionID},
+		labelNamespace:   namespace,
+		labelEventhub:    eventhub,
+		labelPartitionID: partitionID},
 		float64(seqMin))
 
 	s.recorder.RecordMetric(EventhubPartitionSequenceNumberMax, map[string]string{
-		"eh_namespace": namespace,
-		"eventhub":     eventhub,
-		"partition_id": partitionID},
+		labelNamespace:   namespace,
+		labelEventhub:    eventhub,
+		labelPartitionID: partitionID},
 		float64(seqMax))
 }
 
 func (s *service) RecordEventhubSequenceNumberSum(namespace, eventhub string, seqMin, seqMax int64) {
 	s.recorder.RecordMetric(EventhubSequenceNumberMinSum, map[string]string{
-		"eh_namespace": namespace,
-		"eventhub":     eventhub},
+		labelNamespace: namespace,
+		labelEventhub:  eventhub},
 		float64(seqMin))
 
 	s.recorder.RecordMetric(EventhubSequenceNumberMaxSum, map[string]string{
-		"eh_namespace": namespace,
-		"eventhub":     eventhub},
+		labelNamespace: namespace,
+		labelEventhub:  eventhub},
 		float64(seqMax))
 }
 
@@ -77,26 +77,26 @@ func (s *service) RecordConsumerGroupInfo(namespace, eventhub string, consumerGr
 	}
 
 	s.recorder.RecordMetric(ConsumerGroupInfo, map[string]string{
-		"eh_namespace":   namespace,
-		"eventhub":       eventhub,
-		"consumer_group": consumerGroup,
-		"state":          state},
+		labelNamespace:     namespace,
+		labelEventhub:      eventhub,
+		labelConsumerGroup: consumerGroup,
+		"state":            state},
 		value)
 }
 
 func (s *service) RecordConsumerGroupOwners(namespace, eventhub, consumerGroup string, ownerCount int) {
 	s.recorder.RecordMetric(ConsumerGroupOwners, map[string]string{
-		"eh_namespace":   namespace,
-		"eventhub":       eventhub,
-		"consumer_group": consumerGroup},
+		labelNamespace:     namespace,
+		labelEventhub:      eventhub,
+		labelConsumerGroup: consumerGroup},
 		float64(ownerCount))
 }
 
 func (s *service) RecordConsumerGroupEvents(namespace, eventhub, consumerGroup string, eventCount int64) {
 	s.recorder.RecordMetric(ConsumerGroupEventsSum, map[string]string{
-		"eh_namespace":   namespace,
-		"eventhub":       eventhub,
-		"consumer_group": consumerGroup},
+		labelNamespace:     namespace,
+		labelEventhub:      eventhub,
+		labelConsumerGroup: consumerGroup},
 		float64(eventCount))
 }
 
@@ -109,28 +109,28 @@ func (s *service) RecordConsumerGroupPartitionOwner(namespace, eventhub, consume
 	}
 
 	s.recorder.RecordMetric(ConsumerGroupPartitionOwner, map[string]string{
-		"eh_namespace":   namespace,
-		"eventhub":       eventhub,
-		"consumer_group": consumerGroup,
-		"partition_id":   partitionID,
-		"owner":          owner},
+		labelNamespace:     namespace,
+		labelEventhub:      eventhub,
+		labelConsumerGroup: consumerGroup,
+		labelPartitionID:   partitionID,
+		"owner":            owner},
 		value)
 }
 
 func (s *service) RecordConsumerGroupPartitionLag(namespace, eventhub, consumerGroup, partitionID string, lag int64) {
 	s.recorder.RecordMetric(ConsumerGroupPartitionLag, map[string]string{
-		"eh_namespace":   namespace,
-		"eventhub":       eventhub,
-		"consumer_group": consumerGroup,
-		"partition_id":   partitionID},
+		labelNamespace:     namespace,
+		labelEventhub:      eventhub,
+		labelConsumerGroup: consumerGroup,
+		labelPartitionID:   partitionID},
 		float64(lag))
 }
 
 func (s *service) RecordConsumerGroupLag(namespace, eventhub, consumerGroup string, lag int64) {
 	s.recorder.RecordMetric(ConsumerGroupLag, map[string]string{
-		"eh_namespace":   namespace,
-		"eventhub":       eventhub,
-		"consumer_group": consumerGroup},
+		labelNamespace:     namespace,
+		labelEventhub:      eventhub,
+		labelConsumerGroup: consumerGroup},
 		float64(lag))
 }
 
